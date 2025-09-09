a081a1ed8cfb4c1cb51eaf39a227e54e6ffe773a
hello <hello@qq.com>
2025-09-09 10:39:03 +0800

    jpush

--- ios/Runner/AppDelegate.h
+++ ios/Runner/AppDelegate.h
@@ -1,6 +1,16 @@
 #import <Flutter/Flutter.h>
 #import <UIKit/UIKit.h>
 
+#define DEMO_VERSION 2.0.0
+
+
+
+
 @interface AppDelegate : FlutterAppDelegate
+
+@property (strong, nonatomic) UIWindow *window;
+
+/// 是否允许旋转
+@property (assign, nonatomic) BOOL allow;
 
 @end
